7c994b2155608e4da3c9e191dd8ef89e8c3676ce
Julian Bogdani <jbogdani@gmail.com>
2023-03-27 12:26:46 +0200

    Aggiunto esempio di utlizzo

--- test.mjs
+++ test.mjs
@@ -1,3 +1,5 @@
-import getBiblioLocal from "./getBiblioLocal.mjs";
+import getBiblio from "./getBiblio.mjs"; // live
+import getBiblioLocal from "./getBiblioLocal.mjs"; // test
 
-console.log(await getBiblioLocal());
+console.log(await getBiblio()); // live
+console.log(await getBiblioLocal()); // test
