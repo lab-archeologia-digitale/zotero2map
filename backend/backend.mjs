214ff3f0d050c7f2f127060d90785bc8c9152bcd
Julian Bogdani <jbogdani@gmail.com>
2023-04-26 09:38:51 +0200

    Cleaned backend

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -6,10 +6,8 @@ import mapBiblio2Onto from "./mapBiblio2Onto.mjs";
 const ontologyPath = "./data/map.geojson";
 const ontology = JSON.parse(fs.readFileSync(ontologyPath));
 
-let bibliography = await getBiblio();
+const bibliography = await getBiblio();
 
 const test = mapBiblio2Onto(bibliography, ontology);
-// const test2 = mapBiblio2Onto2(bibliography, ontology);
 
 fs.writeFileSync("data/mappedBiblio.geojson", JSON.stringify(test, null, 2));
-// fs.writeFileSync("data/test.geojson", JSON.stringify(test2, null, 2));
